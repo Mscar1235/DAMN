a6c43b071eaa5e162a240843428817a1fe414caf
Mscar1235 <129341137+Mscar1235@users.noreply.github.com>
2023-03-31 00:11:28 -0400

    Add files via upload

---
+++ DAMNScript.js
@@ -0,0 +1,25 @@
+
+document.addEventListener('click', e => {
+    const isDropdownButton = e.target.matches("[data-dropdown-button]")
+    if (!isDropdownButton && e.target.closest('[data-dropdown]') != null)
+        return
+
+    if (!isDropdownButton) {
+        document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+            dropdown.classList.remove('active')
+        })
+    }
+
+    let currentDropdown
+    if (isDropdownButton) {
+        currentDropdown = e.target.closest('[data-dropdown]')
+        currentDropdown.classList.toggle('active')
+    }
+
+})
+
+
+
+
+
+
